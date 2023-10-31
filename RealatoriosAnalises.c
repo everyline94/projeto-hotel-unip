6b4a46e8148497ac94a2fc936d61bd0eac620a6f
1GabrielKasahara <132974759+1GabrielKasahara@users.noreply.github.com>
2023-10-30 22:46:49 -0300

    RealatoriosAnalises.c

--- RealatoriosAnalises.c
+++ RealatoriosAnalises.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Definição da estrutura de dados para uma reserva
+
 typedef struct {
     int id;
     int quarto_id;
     float valor;
-    char data_checkin[11];  // Formato: "yyyy-mm-dd"
-    char data_checkout[11]; // Formato: "yyyy-mm-dd"
+    char data_checkin[11];  
+    char data_checkout[11]; 
 } Reserva;
 
-// Função para calcular a ocupação de um hotel
+
 float calcularOcupacaoHotel(Reserva reservas[], int numReservas, int hotelId) {
     int quartosOcupados = 0;
     for (int i = 0; i < numReservas; i++) {
@@ -23,7 +23,7 @@ float calcularOcupacaoHotel(Reserva reservas[], int numReservas, int hotelId) {
     return (float)quartosOcupados / numReservas * 100;
 }
 
-// Função para calcular a receita gerada por um hotel
+// Calcular a receita gerada por um hotel
 float calcularReceitaHotel(Reserva reservas[], int numReservas, int hotelId) {
     float receita = 0;
     for (int i = 0; i < numReservas; i++) {
@@ -42,11 +42,11 @@ int main() {
 
     int hotelId = 1;
 
-    // Calcular a ocupação do hotel
+    
     float ocupacao = calcularOcupacaoHotel(reservas, numReservas, hotelId);
     printf("Ocupação do Hotel %d: %.2f%%\n", hotelId, ocupacao);
 
-    // Calcular a receita gerada pelo hotel
+    
     float receita = calcularReceitaHotel(reservas, numReservas, hotelId);
     printf("Receita do Hotel %d: R$%.2f\n", hotelId, receita);
 
