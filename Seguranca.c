a366203a6d436320aa62d742711d687d012efe99
1GabrielKasahara <132974759+1GabrielKasahara@users.noreply.github.com>
2023-10-30 22:50:23 -0300

    Seguranca.c

--- Seguranca.c
+++ Seguranca.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-// Definição da estrutura de dados para um hóspede
+
 typedef struct {
     int id;
     char nome[50];
@@ -10,7 +10,7 @@ typedef struct {
     char senha[100];  
 } Hospede;
 
-// Função para verificar o consentimento do hóspede
+
 bool obterConsentimento() {
     char resposta[5];
     printf("Você concorda com o compartilhamento dos seus dados? (sim/nao): ");
@@ -30,12 +30,12 @@ int main() {
     printf("Informe sua senha: ");
     scanf("%s", h.senha);
 
-    // Verificar se o hóspede concorda com o compartilhamento de dados
+   
     if (obterConsentimento()) {
-        // Armazenar os dados do hóspede em algum local seguro
+        
         printf("Seus dados foram armazenados com segurança.\n");
     } else {
-        // O hóspede não concorda com o compartilhamento de dados
+        
         printf("Seus dados não serão compartilhados.\n");
     }
 
